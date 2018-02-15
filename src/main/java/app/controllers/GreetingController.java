687f9ce5bbdd18b322af980f30d5c8e29104c2de
Ciobanu C <cristi.ciobanu@scai-consulting.grupposcai.it>
2018-02-15 17:49:24 +0100

    add ApiCall

--- src/main/java/app/controllers/GreetingController.java
+++ src/main/java/app/controllers/GreetingController.java
@@ -7,19 +7,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import app.models.Beer;
+import app.services.ApiCall;
 
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
-    public String greeting(Model model) {
-    	
-    	RestTemplate restTemplate = new RestTemplate();
-    	Beer[] lista = restTemplate.getForObject(
-				"https://api.punkapi.com/v2/beers/random", Beer[].class);
-    	
-        model.addAttribute("info", lista[0]);
-        return "greeting";
-    }
+//    @RequestMapping("/greeting")
+//    public String greeting(Model model) {
+//    	
+//    	RestTemplate restTemplate = new RestTemplate();
+//    	Beer[] lista = restTemplate.getForObject(
+//				"https://api.punkapi.com/v2/beers/random", Beer[].class);
+//    	
+//        model.addAttribute("info", lista[0]);
+//        return "greeting";
+//    }
+	
+	 @RequestMapping("/greeting")
+	    public String greeting(Model model) {
+		 
+		 	ApiCall x = new ApiCall("https://api.punkapi.com/v2/beers/random", "a");
+	    	
+	        model.addAttribute("info", x.getResultMulti()[0]);
+	        return "greeting";
+	    }
 
 }
