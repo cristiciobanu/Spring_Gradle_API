7f488c60b5417a8d9970a137c89948fbe3b2a1d4
Ciobanu C <cristi.ciobanu@scai-consulting.grupposcai.it>
2018-02-15 16:04:47 +0100

    refactor cartelle

--- src/main/java/app/controllers/GreetingController.java
+++ src/main/java/app/controllers/GreetingController.java
@@ -1,10 +1,12 @@
-package hello;
+package app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+
+import app.models.Beer;
 
 @Controller
 public class GreetingController {
